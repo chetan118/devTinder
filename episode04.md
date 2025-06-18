54ce112ef5ba20fad89933c5b54a1823a4016aeb
chetan118 <chetan.sakpal@gmail.com>
2025-06-18 20:06:29 +0530

    Episode-04 | Routing and Request Handlers
    - Playing with the routes
    
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

--- episode04.md
+++ episode04.md
@@ -8,3 +8,7 @@
 - Order of the routes matter
 - Install Postman and makes a workspace, a collection and a request to test /test route
 - Write methods to handle GET, POST, PUT, PATCH, DELETE API calls and test them from Postman
+- Explore routing and use of ?, +, (), \* in the routes (Express 5.x.x does not support this)
+- Use of regex in routes /a/, /.\*fly$/
+- Reading the query params in the routes
+- Reading the dynamic routes
--- src/app.js
+++ src/app.js
@@ -2,38 +2,13 @@ const express = require("express");
 
 const app = express();
 
-// this will only handle GET call to /user
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Akshay", lastname: "Saini" });
-});
-
-// this will only handle POST call to /user
-app.post("/user", (req, res) => {
-  // Save data to the database
-  res.send("User Created");
-});
-
-// this will only handle PUT call to /user
-app.put("/user", (req, res) => {
-  // Fetch the user from the database and update it
-  res.send("User Updated");
-});
-
-// this will only handle PATCH call to /user
-app.patch("/user", (req, res) => {
-  // Fetch the user from the database and update a specific property
-  res.send("User Property Updated");
-});
-
-// this will only handle DELETE call to /user
-app.delete("/user", (req, res) => {
-  // Delete user from the database"
-  res.send("User Deleted");
-});
-
-// this will match all the HTTP Api method
-app.use("/test", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Hello from the server!");
+});
+
+app.get("/user/:userId/:name/:pass", (req, res) => {
+  console.log(req.params);
+  res.send({ firstname: "Akshay", lastname: "Saini" });
 });
 
 app.listen(7777, () => {
