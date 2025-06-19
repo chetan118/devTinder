4e2a678ac459e82fa06a32705c9ad49273f87e1d
chetan118 <chetan.sakpal@gmail.com>
2025-06-19 08:43:55 +0530

    Episode-05 | Middlewares & Error Handlers
    - Playing around with multiple route handlers
    
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

---
+++ episode05.md
@@ -0,0 +1,6 @@
+## Episode-05 | Middlewares & Error Handlers
+
+- Multiple Route Handlers - play with the code
+- next()
+- next function and errors along with res.send
+- app.use("/route", rH1, [rH2, rH3], rH4, rH5);
--- src/app.js
+++ src/app.js
@@ -2,14 +2,35 @@ const express = require("express");
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
-
-app.get("/user/:userId/:name/:pass", (req, res) => {
-  console.log(req.params);
-  res.send({ firstname: "Akshay", lastname: "Saini" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Route Handler 1");
+    // res.send("Response 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 2");
+    // res.send("Response 2");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Route Handler 3");
+      // res.send("Response 3");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Route Handler 4");
+      // res.send("Response 4");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("Route Handler 5");
+    res.send("Response 5");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is running and listening on port 7777...");
