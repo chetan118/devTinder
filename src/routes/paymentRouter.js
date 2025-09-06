46a5e95401caed49b1f00ce20b6073cd32a55019
chetan118 <chetan.sakpal@gmail.com>
2025-09-06 22:40:33 +0530

    Create order on razorpay
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

--- src/routes/paymentRouter.js
+++ src/routes/paymentRouter.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const { razorpayInstance } = require("../utils/razorpay");
 
 const paymentRouter = express.Router();
 
 paymentRouter.post("/create", userAuth, async (req, res) => {
-  console.log("Payment Creation Logic");
+  try {
+    console.log("Payment Creation Logic");
+    const order = await razorpayInstance.orders.create({
+      amount: 50000,
+      currency: "INR",
+      receipt: "receipt#1",
+      partial_payment: false,
+      notes: {
+        firstName: "value1",
+        lastName: "value2",
+        membershipType: "silver",
+      },
+    });
+    // save the order in the database
+    console.log(order);
+    // return back the order details to the frontend from where this endpoint was called
+    res.json({ order });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: err.message });
+  }
 });
 
 module.exports = { paymentRouter };
