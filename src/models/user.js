8426486d7d3aacaeff8163602b8a1ff955fca7f8
chetan118 <chetan.sakpal@gmail.com>
2025-09-09 10:00:57 +0530

    Razorpay Payment Verification Webhook API
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

--- src/models/user.js
+++ src/models/user.js
@@ -79,6 +79,13 @@ const userSchema = new mongoose.Schema(
     skills: {
       type: [String],
     },
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
+    membershipType: {
+      type: String,
+    },
   },
   {
     timestamps: true,
--- src/routes/paymentRouter.js
+++ src/routes/paymentRouter.js
@@ -3,6 +3,10 @@ const { userAuth } = require("../middlewares/auth");
 const { instance: razorpayInstance } = require("../utils/razorpay.js"); // destructure and rename the razorpay instance object
 const { Payment } = require("../models/payment");
 const { membershipAmount } = require("../utils/constants.js");
+const {
+  validateWebhookSignature,
+} = require("razorpay/dist/utils/razorpay-utils.js");
+const { User } = require("../models/user.js");
 
 const paymentRouter = express.Router();
 
@@ -36,6 +40,40 @@ paymentRouter.post("/create", userAuth, async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.status(500).json({ msg: err.message });
+  }
+});
+
+paymentRouter.post("/webhook", async (req, res) => {
+  try {
+    const webhookSignature = req.get("X-Razorpay-Signature");
+    console.log("Webhook Signature", webhookSignature);
+    const isWebhookValid = validateWebhookSignature(
+      JSON.stringify(req.body),
+      webhookSignature,
+      process.env.RAZORPAY_WEBHOOK_SECRET
+    );
+    if (!isWebhookValid) {
+      return res.status(400).json({ msg: "Webhook signature is invalid" });
+    }
+    console.log("Valid Webhook Signature");
+    const paymentDetails = req.body.payload.payment.entity;
+
+    const payment = await Payment.findOne({ orderId: paymentDetails.order_id });
+    payment.status = paymentDetails.status;
+    await payment.save();
+    console.log("Payment saved", payment);
+
+    if (req.body.event === "payment.captured") {
+      const user = await User.findOne({ _id: payment.userId });
+      user.isPremium = true;
+      user.membershipType = payment.notes.membershipType;
+      await user.save();
+      console.log("User saved", user);
+    }
+
+    res.status(200).json({ msg: "Webhook received successfully" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
   }
 });
 
