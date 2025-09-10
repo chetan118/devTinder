3eed81c0e8bd54b42eb944ade52b2e65a76f69e3
chetan118 <chetan.sakpal@gmail.com>
2025-09-10 20:29:26 +0530

    Sending messages using Socket.io
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,5 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -7,9 +8,24 @@ const initializeSocket = (server) => {
     },
   });
 
+  const getSecretRoomId = (userId, targetUserId) => {
+    return crypto
+      .createHash("sha256")
+      .update([userId, targetUserId].sort().join("_"))
+      .digest("hex");
+  };
+
   io.on("connection", (socket) => {
-    socket.on("joinChat", () => {});
-    socket.on("sendMessage", () => {});
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log(firstName + " joined the room " + roomId);
+      socket.join(roomId);
+    });
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log(firstName + " : " + text);
+      io.to(roomId).emit("messageReceived", { firstName, text });
+    });
     socket.on("disconnect", () => {});
   });
 };
