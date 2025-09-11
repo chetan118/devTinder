0f26dbc12d78d0d76cd3aa8b8049f002faa882f3
chetan118 <chetan.sakpal@gmail.com>
2025-09-11 23:47:24 +0530

    Created a chat schema and storing messages in the database collection when the server receives a new message
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
+const { Chat } = require("../models/chat.js");
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -21,11 +22,34 @@ const initializeSocket = (server) => {
       console.log(firstName + " joined the room " + roomId);
       socket.join(roomId);
     });
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      console.log(firstName + " : " + text);
-      io.to(roomId).emit("messageReceived", { firstName, text });
-    });
+    socket.on(
+      "sendMessage",
+      async ({ firstName, userId, targetUserId, text }) => {
+        try {
+          const roomId = getSecretRoomId(userId, targetUserId);
+          console.log(firstName + " : " + text);
+
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+          await chat.save();
+
+          io.to(roomId).emit("messageReceived", { firstName, text });
+        } catch (err) {
+          console.log("Error saving message to the database", err);
+        }
+      }
+    );
     socket.on("disconnect", () => {});
   });
 };
