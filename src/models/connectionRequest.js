4415601a9247702f3d2a050602bf217ad2d90c3b
chetan118 <chetan.sakpal@gmail.com>
2025-12-27 11:00:00 +0530

    fix: improve self-connection-request error message

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -29,7 +29,7 @@ connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
   if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
-    throw new Error("Cannot send a connection request to yourself");
+    throw new Error("You cannot send a connection request to yourself!");
   }
   next();
 });
