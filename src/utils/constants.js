a34437517e7dddef9a5924c7a943547403537bd8
chetan118 <chetan.sakpal@gmail.com>
2025-12-31 09:30:00 +0530

    chore: add COOKIE_EXPIRY_MS derived constant to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,6 @@ const membershipAmount = {
 
 const COOKIE_EXPIRY_DAYS = 7;
 
-module.exports = { membershipAmount, COOKIE_EXPIRY_DAYS };
+const COOKIE_EXPIRY_MS = COOKIE_EXPIRY_DAYS * 24 * 60 * 60 * 1000;
+
+module.exports = { membershipAmount, COOKIE_EXPIRY_DAYS, COOKIE_EXPIRY_MS };
