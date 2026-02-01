7675608d4d66ebc37b1ffe35a6e1afcb1d0426ec
chetan118 <chetan.sakpal@gmail.com>
2026-02-01 12:00:00 +0530

    docs: add comment explaining SES region selection

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -1,5 +1,6 @@
 const { SESClient } = require("@aws-sdk/client-ses");
 // Set the AWS Region.
+// Using ap-south-1 (Mumbai) for lower latency with India-based users.
 const REGION = "ap-south-1";
 // Create SES service object.
 const sesClient = new SESClient({
