3d8df5b7ea99d199d34c466a3d1a646918afed0d
chetan118 <chetan.sakpal@gmail.com>
2026-02-06 10:00:00 +0530

    fix: throw descriptive error when Razorpay env vars are missing

--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -1,5 +1,9 @@
 const Razorpay = require("razorpay");
 
+if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+  throw new Error("Razorpay credentials missing: set RAZORPAY_KEY_ID and RAZORPAY_KEY_SECRET env vars");
+}
+
 const instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
