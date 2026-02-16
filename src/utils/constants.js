fc825abc2e0ee9cc28e362ecd80158be1aa14209
chetan118 <chetan.sakpal@gmail.com>
2026-02-16 14:00:00 +0530

    chore: add MAX_FEED_PAGE_SIZE constant to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,4 +7,6 @@ const COOKIE_EXPIRY_DAYS = 7;
 
 const COOKIE_EXPIRY_MS = COOKIE_EXPIRY_DAYS * 24 * 60 * 60 * 1000;
 
-module.exports = { membershipAmount, COOKIE_EXPIRY_DAYS, COOKIE_EXPIRY_MS };
+const MAX_FEED_PAGE_SIZE = 10;
+
+module.exports = { membershipAmount, COOKIE_EXPIRY_DAYS, COOKIE_EXPIRY_MS, MAX_FEED_PAGE_SIZE };
